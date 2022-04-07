a0022c3f3df370bb124ece840dad252215080594
amostalong <amostalong@126.com>
2022-04-07 23:43:35 +0800

    update premake

--- premake5.lua
+++ premake5.lua
@@ -10,12 +10,27 @@ project "GLFW"
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
 		"src/glfw_config.h",
+	
+		
+		"src/internal.h",
+		"src/platform.c",
+		"src/platform.h",
+		"src/mappings.h",
+	
 		"src/context.c",
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
 		"src/vulkan.c",
-		"src/window.c"
+		"src/window.c",
+		
+		"src/null_platform.h",
+		"src/null_joystick.h",
+		"src/null_init.c",
+
+		"src/null_monitor.c",
+		"src/null_window.c",
+		"src/null_joystick.c",
 	}
 	filter "system:linux"
 		pic "On"
@@ -54,6 +69,7 @@ project "GLFW"
 			"src/win32_time.c",
 			"src/win32_thread.c",
 			"src/win32_window.c",
+			"src/win32_module.c",
 			"src/wgl_context.c",
 			"src/egl_context.c",
 			"src/osmesa_context.c"
